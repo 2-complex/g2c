1f5a6af8e9dfba0dbf0ecc62967d0e51c65812ed
Peterson Trethewey <peterson.trethewey@gmail.com>
2013-04-07 18:34:29 -0700

    adding worldfile to examples

---
+++ examples/worldfile.cpp
@@ -0,0 +1,33 @@
+
+#include "spriteenvironment.h"
+#include "sprites.h"
+
+#include <stdio.h>
+
+class MySpriteEnvironment : public SpriteEnvironment {
+public:
+	char* filename;
+
+private:
+	void init();
+};
+
+void MySpriteEnvironment::init()
+{
+	bank.initSerializableWithPath(&world, filename);
+	printf( "%s\n", world.serialize().c_str() );
+}
+
+int main(int argc, char** args)
+{
+	MySpriteEnvironment e;
+	
+	e.filename = (char*)"my.world";
+	if( argc > 1 )
+		e.filename = args[1];
+	
+	e.mainLoop();
+	
+	return 0;
+}
+
