add99966dbc8a5bf373e8e89128b19db81193585
Peterson Trethewey <peterson.trethewey@gmail.com>
2014-10-26 11:34:36 -0700

    Added steam example, fixed ios trampoline (which broke after moving it to a new directory relative to g2c) and added animation to panenvironment

---
+++ examples/steam.cpp
@@ -0,0 +1,165 @@
+
+#include "panenvironment.h"
+#include "graphics.h"
+
+using namespace std;
+
+struct Particle
+{
+    Particle(const Vec3& inCenter, double inRadius)
+    {
+        center = inCenter;
+        radius = inRadius;
+
+        deltaCenter = Vec3(
+            (randInt(110) - 50) / 1500.0,
+            (randInt(110) - 50) / 1500.0,
+            .3);
+        deltaRadius = 0.1;
+    }
+
+    Vec3 center;
+    double radius;
+
+    Vec3 deltaCenter;
+    double deltaRadius;
+};
+
+class SteamEnvironment : public PanEnvironment
+{
+    Buffer buffer;
+    IndexBuffer indexBuffer;
+    Geometry geometry;
+    Effect effect;
+    Assumption material;
+    mutable Assumption particle;
+    mutable Assumption camera;
+    Shape shape;
+
+    double last_t;
+    vector<Particle> particles;
+
+    void init();
+    void step(double t);
+    void draw() const;
+};
+
+void SteamEnvironment::init()
+{
+    animate = true;
+
+    float vertexArray[] =
+    {
+         -1.0, -1.0, 0.0,
+          1.0, -1.0, 0.0,
+          1.0,  1.0, 0.0,
+         -1.0,  1.0, 0.0,
+    };
+
+    int indexArray[] =
+    {
+        0,1,2,
+        0,2,3,
+    };
+
+    buffer.set(vertexArray, sizeof(vertexArray) / sizeof(float));
+    indexBuffer.set(indexArray, sizeof(indexArray) / sizeof(int));
+
+    geometry["position"] = Field(&buffer, 3, 3, 0);
+    geometry.indices = &indexBuffer;
+
+    effect.vertexCode =
+        "\n"
+        "uniform mat4 modelView;\n"
+        "uniform mat4 projection;\n"
+        "uniform vec3 center;\n"
+        "uniform float radius;\n"
+        "\n"
+        "attribute vec3 position;\n"
+        "varying vec3 v_position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = projection * (modelView * vec4(center, 1.0) + vec4(radius * position, 0.0) );\n"
+        "    v_position = position;\n"
+        "}\n";
+
+    effect.fragmentCode =
+        "\n"
+        "uniform vec4 color;\n"
+        "varying vec3 v_position;\n"
+        "uniform float radius;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    gl_FragColor = 0.3 * color / (radius*radius) / (1.0 + 5.0*dot(v_position.xy, v_position.xy));\n"
+        "}\n";
+
+    effect.compile();
+
+    material.effect = &effect;
+
+    shape.geometry = &geometry;
+    shape.assumptions.push_back(&camera);
+    shape.assumptions.push_back(&particle);
+    shape.assumptions.push_back(&material);
+
+    material["color"] = Vec4(1.0, 1.0, 1.1, 1.0);
+    last_t = 0;
+}
+
+void SteamEnvironment::step(double t)
+{
+    if ( t - last_t > 1.0 / 30.0 )
+    {
+        for( vector<Particle>::iterator itr = particles.begin();
+            itr != particles.end();
+            itr++ )
+        {
+            itr->center += itr->deltaCenter;
+            itr->radius += itr->deltaRadius;
+
+            if ( itr->radius > 8 )
+            {
+                itr->center = Vec3(0,0,-10);
+                itr->radius = .5;
+            }
+        }
+
+        if ( particles.size() < 80 )
+        {
+            particles.push_back(
+                Particle( Vec3(0.0, 0.0, -10.0), .5 ) );
+        }
+
+        last_t = t;
+    }
+}
+
+void SteamEnvironment::draw() const
+{
+    camera["modelView"] = getModelView();
+    camera["projection"] = getProjection();
+
+    glDisable(GL_DEPTH_TEST);
+    glBlendFunc(GL_ONE, GL_ONE);
+
+    for( vector<Particle>::const_iterator itr = particles.begin();
+        itr != particles.end();
+        itr++ )
+    {
+        particle["center"] = itr->center;
+        particle["radius"] = itr->radius;
+        shape.draw();
+    }
+}
+
+int main()
+{
+    SteamEnvironment e;
+    e.mainLoop();
+
+    return 0;
+}
+
+
