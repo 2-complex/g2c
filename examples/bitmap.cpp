e944a34df58b99cedc8f4fa5381580aa2be4103b
Peterson Trethewey <peterson@2-complex.com>
2014-12-12 10:10:22 -0800

    Made bitmap example use app instead of environment

--- examples/bitmap.cpp
+++ examples/bitmap.cpp
@@ -1,35 +1,54 @@
-#include "spriteenvironment.h"
 
-class MyEnvironment : public SpriteEnvironment
+#include "sprites.h"
+#include "app.h"
+#include "launch.h"
+#include "transforms.h"
+
+using namespace g2c;
+
+class BitmapApp : public App
 {
-	void draw() const;
-	void init();
-	
-	Bitmap bitmap;
-	Sprite ladybug;
-	Actor cynthia;
+    void draw() const;
+    void init();
+    void setBank(Bank* bank);
+    void reshape(int width, int height);
+
+    Bitmap bitmap;
+    Sprite ladybug;
+    Actor cynthia;
+    RendererGL2 renderer;
+
+    Bank* bank;
 };
 
-void MyEnvironment::init()
+void BitmapApp::setBank(Bank* bank)
 {
-	bank.initBitmapWithPath(&bitmap, "ladybug.png");
-	
-	ladybug.initWithBitmap(bitmap);
-	
-	cynthia.sprite = &ladybug;
+    this->bank = bank;
 }
 
-void MyEnvironment::draw() const
+void BitmapApp::reshape(int width, int height)
 {
-	cynthia.draw();
+    renderer.projection = orthographic(0,width,0,height,-1,1);
 }
 
+void BitmapApp::init()
+{
+    bank->initBitmapWithPath(&bitmap, "ladybug.png");
+    ladybug.initWithBitmap(bitmap);
+    cynthia.sprite = &ladybug;
+    renderer.init();
+    Mesh::renderer = &renderer;
+}
+
+void BitmapApp::draw() const
+{
+    cynthia.draw();
+}
 
 int main()
 {
-	MyEnvironment e;
-	e.mainLoop();
-	
-	return 0;
+    BitmapApp app;
+    launch(&app);
+    return 0;
 }
 
