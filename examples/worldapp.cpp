ebfa9bbad9759ccac370547d4e6d8df6fbfa17f9
Peterson Trethewey <peterson@2-complex.com>
2014-12-26 11:16:19 -0800

    worldapp

--- examples/worldapp.cpp
+++ examples/worldapp.cpp
@@ -1,13 +1,42 @@
 
-#include "worldapp.h"
 #include "sprites.h"
 #include "transforms.h"
 #include "rendererg.h"
+#include "launch.h"
+
 
 using namespace g2c;
+using namespace std;
 
-WorldApp::WorldApp() {}
-WorldApp::~WorldApp() {}
+class WorldApp : public App
+{
+public:
+    WorldApp();
+    virtual ~WorldApp();
+
+    string filename;
+
+    virtual void setBank(Bank* inBank);
+
+    virtual void init();
+    virtual void reshape(int width, int height);
+    virtual void draw() const;
+    virtual void destroy();
+
+private:
+    Bank* bank;
+    World* world;
+    Renderer* renderer;
+};
+
+WorldApp::WorldApp()
+    : filename("bug.world")
+{
+}
+
+WorldApp::~WorldApp()
+{
+}
 
 void WorldApp::setBank(Bank* inBank)
 {
@@ -30,10 +59,10 @@ void WorldApp::init()
     glEnable(GL_ALPHA);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    Sprite::renderer = renderer;
+    Mesh::renderer = renderer;
 
     world->bank = bank;
-    bank->initSerializableWithPath(world, "bug.world");
+    bank->initSerializableWithPath(world, filename.c_str());
     g2clog( "%s\n", world->serialize().c_str() );
 }
 
@@ -42,10 +71,21 @@ void WorldApp::draw() const
     world->draw();
 }
 
-
 void WorldApp::destroy()
 {
     delete world;
     delete renderer;
 }
+
+int main(int argc, char** args)
+{
+    WorldApp app;
+
+    if( argc > 1 )
+        app.filename = args[1];
+
+    launch(&app);
+    return 0;
+}
+
 
